45a93c6f37e8b742ff199a6947f26744bfde7277
bhaaru siva <118578119+SivaBharathiJS@users.noreply.github.com>
2022-12-12 22:57:01 +0530

    Add files via upload

---
+++ loadingtime/pre.js
@@ -0,0 +1,13 @@
+var loader = document.querySelector(".loader")
+
+// window.addEventListener("load", vanish);
+
+// function vanish() {
+//   loader.classList.add("disppear");
+// }
+
+function vanish(){
+
+  loader.style.display = 'none';
+ 
+};
